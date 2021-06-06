69626352d8463a2c4209752ba7db4a2512cdecb3
yutking <77726125+yutking@users.noreply.github.com>
2021-06-06 14:47:25 +0500

    Update insert_and_check_data.pl

--- insert_and_check_data.pl
+++ insert_and_check_data.pl
@@ -9,12 +9,22 @@ $sql1="delete from y";
 $sth1 = $dbh1->prepare($sql1);
 $sth1->execute();
 
-for($i = 1; $i <= 1000000; $i++) {
- 
- $h1{$i} = 1;
- $sql1 = "insert into y (i) values ($i)";
- $sth1=$dbh1->do($sql1) or $h1{$i} = 2;
+for($i = 1; $i <= 10; $i++) {
 
+ $h1{$i} = 1;
+
+ $sql1 = "insert into y (i) values ($i)";
+ $sth1 = $dbh1->prepare($sql1);
+ $sth1->execute();
+ if(!$sth1) {
+  $h1{$i} = 2;
+  if (!$sth1->pg_ready) {
+   undef($sth1);
+   undef($dbh1);
+   $dbh1 = DBI->connect("DBI:Pg:dbname=postgres;host=127.0.0.1;port=5434","postgres","", { RaiseError => 1, AutoCommit => 1 }) or die "Can't connect PostgreSQL server\n";
+  }
+  `/usr/bin/pg_ctlcluster 9.4 c02 promote`
+ }
 }
 
 $dbh2 = DBI->connect("DBI:Pg:dbname=postgres;host=127.0.0.1;port=5434","postgres","", { RaiseError => 1, AutoCommit => 1 }) or die "Can't connect PostgreSQL server\n";
@@ -22,15 +32,15 @@ $dbh2 = DBI->connect("DBI:Pg:dbname=postgres;host=127.0.0.1;port=5434","postgres
 $sql2 = "select i from y";
 $sth2 = $dbh2->prepare($sql2);
 $sth2->execute();
- 
+
 while(@row2 = $sth2->fetchrow_array) {
- 
+
  $h2{$row2[0]} = 1;
 
 }
- 
+
 while (@res = each %h1) {
  if(! exists($h2{$res[0]})) {
-  print "line not found! $res[0] = $res[1]\n"
+  print "row not found! $res[0] = $res[1]\n"
  }
 }
